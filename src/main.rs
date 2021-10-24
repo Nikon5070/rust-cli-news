2e6e9f17fa6d0db3affe7e4e2e83c5619c3a5e9a
Nikita Kuprin <nikon5070@gmail.com>
2021-10-24 22:04:30 +1000

    Refactor newsapi

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,12 @@ use std::env;
 extern crate dotenv;
 
 use dotenv::dotenv;
-use serde::Deserialize;
 use colour::{dark_green, dark_blue};
 use lazy_static::lazy_static;
-
+use newsapi::{get_articles, Articles};
 
 lazy_static! {
     static ref API_KEY: String = env::var("API_KEY").unwrap();
-}
-
-
-#[derive(Deserialize, Debug)]
-struct Articles {
-    articles: Vec<Article>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Article {
-    title: String,
-    url: String,
-}
-
-
-fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
-    let body: String = ureq::get(url)
-        .call()?
-        .into_string()?;
-
-    let articles: Articles = serde_json::from_str(&body)?;
-    Ok(articles)
 }
 
 fn render_articles(articles: &Articles) {
@@ -43,11 +20,10 @@ fn render_articles(articles: &Articles) {
 }
 
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
 
-    let url = &format!("https://newsapi.org/v2/everything?q=tesla&from=2021-09-23&sortBy=publishedAt&apiKey={}", *API_KEY);
+    let url = &format!("https://newsapi.org/v2/everything?q=tesla&from=2021-09-30&sortBy=publishedAt&apiKey={}", *API_KEY);
     let articles = get_articles(url)?;
 
     render_articles(&articles);
