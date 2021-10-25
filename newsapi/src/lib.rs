71239d6931a5f32b326a839da67bb77c06585d97
Nikita Kuprin <nikon5070@gmail.com>
2021-10-25 15:57:43 +1000

    Add errors

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,5 +1,15 @@
-use std::error::Error;
 use serde::Deserialize;
+
+#[derive(thiserror::Error, Debug)]
+pub enum NewsApiError {
+    #[error("Failed fetching articles")]
+    RequestFailed(ureq::Error),
+    #[error("Failed converting response to string")]
+    FailedResponseToString(std::io::Error),
+    #[error("Article parsing failed")]
+    ArticleParseFailed(serde_json::Error),
+}
+
 
 #[derive(Deserialize, Debug)]
 pub struct Articles {
@@ -12,12 +22,12 @@ pub struct Article {
     pub url: String,
 }
 
-pub fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+pub fn get_articles(url: &str) -> Result<Articles, NewsApiError> {
     let body: String = ureq::get(url)
-        .call()?
-        .into_string()?;
+        .call()
+        .map_err(|e| NewsApiError::RequestFailed(e))?.into_string().map_err(|e| NewsApiError::FailedResponseToString(e))?;
 
-    let articles: Articles = serde_json::from_str(&body)?;
+    let articles: Articles = serde_json::from_str(&body).map_err(|e| NewsApiError::ArticleParseFailed(e))?;
     Ok(articles)
 }
 
